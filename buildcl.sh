1f87836cf49b798482a2f7e654c0730cd70ecb4f
Stefan Vigerske <svigerske@gams.com>
2017-04-26 14:09:11 +0200

    start a script for building on windows
    - too lazy to make a proper makefile for this rare usecase

---
+++ buildcl.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+GAMSPATH="/path/to/gams"
+WORHPPATH="libworhp_vs2013_1.9-1_16_10/vs2013-Release"
+
+GAMSAPI="$GAMSPATH/apifiles/C/api"
+
+cl -nologo -c "$GAMSAPI/gmomcc.c" -I"$GAMSAPI"
+cl -nologo -c "$GAMSAPI/gevmcc.c" -I"$GAMSAPI"
+#TODO SRCDIR is wrong, but should not be used with Worhp 2 anymore
+cl -nologo -c "gamsworhp.c" -I"$GAMSAPI" -I"$WORHPPATH/include/worhp" -DSRCDIR=\"\"
+
+link -nologo -out:gamsworhp.exe gmomcc.obj gevmcc.obj gamsworhp.obj "$WORHPPATH"/bin32/libworhp.lib
